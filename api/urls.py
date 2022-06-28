1c7ae5b6afe8791527b7d31e33f07d6790fc227a
khazratoff <iiibnmibnh@gmail.com>
2022-06-28 16:20:23 +0500

    del-api

--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from api import views
-urlpatterns=[
-    path('',views.GetRoutes),
-    path('courses/',views.getCourses),
-    path('courses/<str:pk>/',views.getCourse)
-]
